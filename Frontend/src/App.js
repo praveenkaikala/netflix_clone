b1cc6c23bac1b425093fcf91fa6ea61f61e28f63
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-10-29 06:35:42 +0530

    route issue

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,7 +12,7 @@ function App() {
     <div>
     {/* <Body/> */}
     <Routes>
-      <Route path='/sign  in' element={<Loginpage/>}/>
+      <Route path='/signin' element={<Loginpage/>}/>
       <Route path='/signup' element={<SignUpPage/>}/>
       <Route path='/' element={<HomePage/>}/>
     </Routes>
